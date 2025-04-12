b40f4ebe87ffe30c88a2d2fe0e7e553892019e5b
hky035 <hky035@gmail.com>
2025-04-12 19:14:14 +0900

    test: @SpringBootTest 어노테이션 주석 처리

--- src/test/java/kr/co/yeogiga/YeogigaApplicationTests.java
+++ src/test/java/kr/co/yeogiga/YeogigaApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.yeogiga;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class YeogigaApplicationTests {
 
     @Test
